157182bcb5699ee4104689e6a634c14bf4dd27c0
karthikkosireddi-g <58488937+karthikkosireddi-g@users.noreply.github.com>
2019-12-05 12:08:26 -0800

    with changes to Laura's API code (#7)
    * 1-myChangeKK
    
    jus a test
    
    * public-change-byKK
    
    * commit from PyCharm
    
    * commit from PyCharm - by KK
    
    * commit from PyCharm - by KK
    
    * commit from PyCharm - by KK
    
    * commit from PyCharm - by KK
    
    * from KK - changed Laura's code
    
    * from KK - changed Laura's code

--- 40-Laura_API.py
+++ 40-Laura_API.py
@@ -10,6 +10,8 @@ import json
 
 bURL = "https://reqres.in/api/users/"
 uURL = []
+
+=======
 '''
 for uID in range(0, 25) :
     uURL.append(bURL + str(uID))
@@ -24,19 +26,31 @@ for eachURL in uURL :
 # patch url: https://reqres.in/api/users/2
 # patch json file: patch.json
 
+
 # below is the patch function
 def patch_reqres(filename,url):
     f1 = open(filename,"r")
     f1Data = json.load(f1)
     PatchReq = requests.patch(pURL,f1Data)
     print(PatchReq.text)
+
+    print(PatchReq.status_code)
+    print(PatchReq.json())
+    f1.close()
+    return PatchReq
+=======
     return PatchReq
     f1.close()
+
 
 
 # here we use the patch function
 filename = "patch.json"
 pURL = "https://reqres.in/api/users/2"
 
+
+print (patch_reqres(filename,pURL))
+=======
 patch_reqres(filename,pURL)
+
 
